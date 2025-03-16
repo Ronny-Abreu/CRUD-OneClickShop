9f66f510e6fa818e0998acd2c063998a52bb5c9e
SlimDReaper <ronnyduran35@gmail.com>
2025-03-15 20:21:41 -0400

    Agregación de servicios de los productos a la clase ProductoService

--- OneClickShop/OneClickShop.Application/Services/ProductoService.cs
+++ OneClickShop/OneClickShop.Application/Services/ProductoService.cs
@@ -6,6 +6,36 @@ namespace OneClickShop.Application.Services
 {
     public class ProductoService
     {
+        private readonly ProductoRepository _repository;
 
+        public ProductoService(ProductoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Producto>> ObtenerTodos()
+        {
+            return await _repository.ObtenerTodos();
+        }
+
+        public async Task<Producto> ObtenerPorId(int id)
+        {
+            return await _repository.ObtenerPorId(id);
+        }
+
+        public async Task Agregar(Producto producto)
+        {
+            await _repository.Agregar(producto);
+        }
+
+        public async Task Actualizar(Producto producto)
+        {
+            await _repository.Actualizar(producto);
+        }
+
+        public async Task Eliminar(int id)
+        {
+            await _repository.Eliminar(id);
+        }
     }
 }
