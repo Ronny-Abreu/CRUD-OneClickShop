e4d7a88cf63092513e6037d8a696b645f67b5958
SlimDReaper <ronnyduran35@gmail.com>
2025-03-17 23:34:08 -0400

    Creacion archivo Dto para transferencia de datos y estructura

---
+++ OneClickShop/OneClickShop.Api/Dtos/ProductoDtos.cs
@@ -0,0 +1,11 @@
+﻿namespace OneClickShop.Api.Dtos
+{
+    public class ProductoDtos
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
